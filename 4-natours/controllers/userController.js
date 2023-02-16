35ec07f5b96503615452ddedbf040caa9d83dbdc
anggawisman <angga.wisman.nhf@gmail.com>
2023-02-16 10:13:13 +0700

    Managing Passwords

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,32 +1,60 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route not yet defined!',
-    })
+const User = require(`../models/userModel`);
+const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  // console.log(req.query);
+
+  // EXECUTE QUERY
+  const features = new APIFeatures(User.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .pagination();
+  const users = await features.query;
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users: users,
+    },
+  });
+});
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  // user.findOne({_id: req.params.id})
+
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
   }
-  
-  exports.getUser = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route not yet defined!',
-    })
-  }
-  
-  exports.createUser = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route not yet defined!',
-    })
-  }
-  exports.updateUser = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route not yet defined!',
-    })
-  }
-  exports.deleteUser = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route not yet defined!',
-    })
-  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route not yet defined!',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route not yet defined!',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route not yet defined!',
+  });
+};
--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const userController = require('./../controllers/userController')
-
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// SIGNUP
+router.post('/signup', authController.signUp);
 
-router.route('/')
-    .get(userController.getAllUsers)
-    .post(userController.createUser);
+// REST FORMAT because possibility of a system administrator updating, deleting, getting all the users based on their ID.
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
-router.route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
-
